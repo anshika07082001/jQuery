df0ca6cf1a9fa8da30a18673d6290a66e3c2fc66
anshikachaurasiya-cedcoss <anshikachaurasiya@cedcoss.com>
2022-08-03 19:16:19 +0530

    modified task 2

--- jqtask2.js
+++ jqtask2.js
@@ -1,11 +1,14 @@
 $(document).ready(function(){
     $("#btn1").click(function(){
-        $("#mainDiv").append("<div id='genDiv'><input placeholder='Enter Class' id='classInp'><input placeholder='Enter Board' id='BoardInp'><input placeholder='Enter Marks' id='marksInp'><input placeholder='Enter Divison' id='divisonInp'><button class='btn'>-</button></div>");
+        $("#mainDiv").append("<div><input placeholder='Enter Class' id='classInp'><input placeholder='Enter Board' id='BoardInp'><input placeholder='Enter Marks' id='marksInp'><input placeholder='Enter Divison' id='divisonInp'><button id='btn11'>+</button><button id='btn'>-</button></div>");
     });
     $("#btn2").click(function(){
         $("#formDiv").remove();
     });
-    $("#mainDiv").on("click",".btn",(function(){
+    $("#mainDiv").on("click","#btn",(function(){
         $(this).parent().remove();
     }));
+    $("#mainDiv").on("click","#btn11",(function(){
+        $(this).parent().append("<div><input placeholder='Enter Class' id='classInp'><input placeholder='Enter Board' id='BoardInp'><input placeholder='Enter Marks' id='marksInp'><input placeholder='Enter Divison' id='divisonInp'><button id='btn11'>+</button><button id='btn'>-</button></div>")
+    }))
 });
