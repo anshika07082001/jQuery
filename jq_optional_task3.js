3c268c3a31323b22ba3830d77c77583847969c88
anshikachaurasiya-cedcoss <anshikachaurasiya@cedcoss.com>
2022-09-29 19:08:25 +0530

    js file

--- jq_optional_task3.js
+++ jq_optional_task3.js
@@ -1,172 +1,102 @@
-$(document).ready(function(){
-$products=[
-    {id:101,name:"name1",quantity:0,mrp:0,tax:0,total:0},
-    {id:102,name:"name2",quantity:0,mrp:0,tax:0,total:0},
-    {id:103,name:"name3",quantity:0,mrp:0,tax:0,total:0},
-    {id:104,name:"name4",quantity:0,mrp:0,tax:0,total:0},
-    {id:105,name:"name5",quantity:0,mrp:0,tax:0,total:0}
-]
-$min=0;
-$tax5=$tax12=$tax18=$tax28=$gTotal=0;
-console.log("length="+$("tbody").children().length)
-$("#tabItems").on("click","#btnAdd",function(){
-    if($(this).parent().parent().find(".inpQuantity").val()=="" || $(this).parent().parent().find(".inpMRP").val()==""){
-    alert("Please fill both MRP and Quantity to add new row")
-    }
-    else{
-    $arr=[];
-    $arr=$(this).parent().parent().parent().children().each(function(){
-        $arr.push($(this))
-    })
-    
-    if($arr.length<$products.length+1){
-        console.log($index=$(this).parent().parent())
-        $($index).after("<tr><td><select class='selProducts'><option>--Select Product--</option> <option>101 name1</option><option>102 name2</option><option>103 name3</option><option>104 name4</option> <option>105 name5</option></select></td><td><input type='text' class='inpQuantity' disabled/></td><td><input type='text' class='inpMRP' disabled /></td><td><select id='selTax' disabled> <option>0%</option><option>5%</option><option>12%</option><option>18%</option><option>28%</option></select></td><td><input type='text' class='inpTotal' disabled /></td><td><button id='btnAdd' disabled><i class='fa-solid fa-plus'></i></button><button id='btnRemove' disabled><i class='fa-solid fa-trash-can'></i></button></td></tr>")
-    }
-    else{
-        return;
-    }
+var Item=[{Item:"111-Product1",Price:"240"},
+{Item:"102-Product2",Price:"540"},{Item:"121-Product3",Price:"840"},{Item:"124-Product4",Price:"140"}];
+var Tax=[{tax:0},{tax:5},{tax:12},{tax:18},{tax:28}];
+var prd=[{"item":"111-Silver","Qty":0,"MRP":0,"TAX":0,"TOTAL":0}];
+populate();
+function optioner(arr,data,value)
+{
+ var temp="";
+ for(var i=0;i<arr.length;i++)
+ {
+    var sign=(data=="tax")?"%": "";
+    var sel= (arr[i][data]==value)?"selected":"";
+    temp+='<option value="'+arr[i][data]+'" '+sel+'>'+arr[i][data]+' '+sign+'</option>';
+ }
+ return temp;
 }
-    })
-
-
-$("#tabItems").on("click","#btnRemove",function(){
-        $arr=[];let $txt;let $index;
-        $arr=$(this).parent().parent().parent().children().each(function(){
-            $arr.push($(this))
-        })
-        console.log($txt=$(this).parent().parent().find("#selTax :selected").text())
-        console.log($index=$(this).parent().parent().find(".selProducts :selected").index());
-        console.log("tax==",$products[$index-1].tax)
-        if($txt=="5%"){
-            $tax5-=$products[$index-1].tax
-            $("#inp5").val($tax5)
-        }
-        else if($txt=="12%"){
-            $tax12-=$products[$index-1].tax
-            $("#inp12").val($tax12)
-        }
-        else if($txt=="18%"){
-            $tax18-=$products[$index-1].tax
-            $("#inp18").val($tax18)
-        }
-        else if($txt=="28%"){
-            $tax28-=$products[$index-1].tax
-            $("#inp28").val($tax28)
-        }
-        else{
-        }
-        $ind=$(this).parent().parent().remove()
+function populate()
+{
+    $("#tbody1").html("");
+    var temp="";
+    for(var i=0;i<prd.length;i++)
+    {
+        temp+='<tr><td><select class="input" id="item" index="'+i+'">'+optioner(Item,"Item",prd[i].item)+'</select></td><td><input index="'+i+'" value="'+prd[i].Qty+'" id="Qty" class="input" type="number"></td><td><input index="'+i+'" class="input" id="MRP" value="'+prd[i].MRP+'" type="number"></td><td><select id="TAX" class="input" index="'+i+'">'+optioner(Tax,"tax",prd[i].TAX)+'</select></td><td><input index="'+i+'" id="TOTAL'+i+'" value="'+prd[i].TOTAL+'" type="number" disabled></td><td><button index="'+i+'" class="add">+</button><button index="'+i+'" class="del"><i class="fa fa-trash" aria-hidden="true"></i></button></td></tr>'        
+    }
+    $("#tbody1").html(temp);
+}
+$("#tbody1").on("click",".add",function()
+{
+    index=parseInt($(this).attr('index'))+1;
+    prd.splice( index, 0, {"item":"111-Silver","Qty":0,"MRP":0,"TAX":0,"TOTAL":0});
+    populate()
+})
+$("#tbody1").on("click",".del",function()
+{
+    index=parseInt($(this).attr('index'));
+    prd.splice( index, 1);
+    populate();
+    taxes();
+})
+function addData(row)
+{
+    let value=row.val();
+    let keyObj=row.attr("id");
+    let i=parseInt(row.attr("index"));
+    $.each(prd[i], function(key, va){
+        if(key==keyObj)
+        {
+            if(keyObj=="item")
+            {
+                prd[i][key]=value;
+            }
+            else
+            {
         
+            prd[i][key]=parseInt(value);
+            prd[i]["TOTAL"]=prd[i].MRP*prd[i].Qty ;
+            $("#TOTAL"+i).val(prd[i]["TOTAL"]);
+            }
+        
+        }
+    });
+}
+$("#tbody1").on("input",".input",function()
+{
+    addData($(this));
+    taxes();
+  
+})
+function taxes()
+{
+    sum=0;
+    prd.map((x)=>sum+=x.TOTAL);
+    $("#Innovice").val(sum);
+    let per0=0;prd.map((x)=>{(x.TAX==0)?per0+=x.TOTAL*0:""});$("#p0").val(per0);
+    let per5=0;prd.map((x)=>{(x.TAX==5)?per5+=x.TOTAL/20:""});$("#p5").val(per5);
+    let per12=0;prd.map((x)=>{(x.TAX==12)?per12+=x.TOTAL*12/100:""});$("#p12").val(per12);
+    let per18=0;prd.map((x)=>{(x.TAX==18)?per18+=x.TOTAL*18/100:""});$("#p18").val(per18);
+    let per28=0;prd.map((x)=>{(x.TAX==28)?per28+=x.TOTAL*28/100:""});$("#p28").val(per28);
+    $(".innovice").hide();
+}
+$("#save").click(function()
+{
+    populateInnovice();
+    $(".p0").text($("#p0").val());
+    $(".p5").text($("#p5").val());
+    $(".p12").text($("#p12").val());
+    $(".p18").text($("#p18").val());
+    $(".p28").text($("#p28").val());
+    $("#subtotal").text($("#Innovice").val());
+    $("#grandTotal").text(eval($("#Innovice").val()+"+"+$("#p0").val()+"+"+$("#p5").val()+"+"+$("#p12").val()+"+"+$("#p18").val()+"+"+$("#p28").val()));
+    $(".innovice").show();
 })
 
-$("#tabItems").on("keyup","input",function(){
-    $min++;
-    $(".selProducts").attr("disabled","disabled")
-    console.log($(this).parent().parent().find("#selTax").removeAttr("disabled"))
-   let $this=$(this);
-   let $index=$(this).parent().parent().index()
-   console.log($index)
-   console.log($idName=$(this).parent().parent().find(".selProducts :selected").text())
-for(i=0;i<$products.length;i++){
-    if($idName==$products[i].id+" "+$products[i].name){
-        var $ind=i;
-       console.log("ind=",$ind)
+function populateInnovice()
+{
+    var temp="";
+    for(var i=0;i<prd.length;i++)
+    {
+        temp+='<tr><td>'+prd[i].item+'</td><td>'+prd[i].MRP+'</td><td>'+prd[i].Qty+'</td><td>'+prd[i].TAX+'%</td><td>'+prd[i].TOTAL+'</td></tr>'        
     }
-}if($ind>=0){
-console.log($calTotal($ind,$this));
+    $("#itbody").html(temp);
 }
-})
-
-$("#tabItems").on("change",".selProducts",function(){
-    let $index=$(this).parent().parent().index()
-    console.log($index)
-    console.log($(this).parent().parent().find(".inpQuantity").removeAttr("disabled"))
-    console.log($(this).parent().parent().find(".inpMRP").removeAttr("disabled"))
-    console.log($(this).parent().parent().find("button").removeAttr("disabled"))
-    
- })
-function $calTotal($args,$argsThis){
-  console.log($products[$args].quantity=$($argsThis).parent().parent().find(".inpQuantity").val()) 
-  console.log($products[$args].mrp=$($argsThis).parent().parent().find(".inpMRP").val()) 
-  $totalNum=0;
-  console.log($products[$args].quantity)
-  console.log($products[$args].mrp)
-  $totalNum=$products[$args].quantity*$products[$args].mrp
-  $products[$args].total=$totalNum;
-  $($argsThis).parent().parent().find(".inpTotal").val($totalNum)
-  $dispTotal()
-  return parseInt($totalNum);
-}
-function $dispTotal(){
-    $amount=0;
-    for(i=0;i<$products.length;i++){
-        $amount+=$products[i].total;
-    }
-    $("#inpAmount").val($amount)
-}
-
-$("#tabItems").on("change","#selTax",function(){
-    console.log($txt=$(this).find(":selected").text());
-    console.log($num=$txt.match(/\d+/)[0])
-    let $idName,$ind;
-    console.log($idName=$(this).parent().parent().find(".selProducts :selected").text())
-for(i=0;i<$products.length;i++){
-    if($idName==$products[i].id+" "+$products[i].name){
-        $ind=i;
-       console.log("ind=",$ind)
-    }
-}   
-    console.log($products[$ind].tax=$products[$ind].total*$num/100);
-    if($txt=="5%"){
-        $tax5+=$products[$ind].tax
-    } 
-    else if($txt=="12%"){
-        $tax12+=$products[$ind].tax
-    } 
-    else if($txt=="18%"){
-        $tax18+=$products[$ind].tax
-    } 
-    else if($txt=="28%"){
-        $tax28+=$products[$ind].tax
-    } 
-    $("#inp5").val($tax5)
-    $("#inp12").val($tax12)
-    $("#inp18").val($tax18)
-    $("#inp28").val($tax28)
-})
-$("#btnSave").click(function(){
-    $displayBill();
-})
-function $displayBill(){
-    if($min==0){
-        alert("Please fill atleast one row to display bill")
-    }
-    else{
-    $("#bill").css("visibility","visible")
-    $cart=[];
-    let $arr=$("tbody").children();
-    $len=$("tbody").children().length;
- for(i=1;i<$arr.length;i++){
-    console.log($name=$("tr").eq(i).find(".selProducts :selected").html())
-    obj={Name:$name}
-    $cart.push(obj)
- }
- console.log($cart)
- for(i=0;i<$cart.length;i++){
-    for(j=0;j<$products.length;j++){
-   if($cart[i].Name==$products[j].id+" "+$products[j].name){
-    $cart[i]=$products[j];
-   }
-    }
- }
- console.log($cart)
-    $gTotal=parseFloat($("#inpAmount").val())+parseFloat($tax5)+parseInt($tax12)+parseFloat($tax18)+parseFloat($tax28);
-    $("#bill").append("<h1>Bill Details</h1>")
-    for(i=0;i<$cart.length;i++){
-        $("#bill").append("<p>"+$cart[i].id+" "+$cart[i].name+" @ "+$cart[i].quantity+" x "+$cart[i].mrp+" : "+$cart[i].total+"</p>")
-    }
-    $("#bill").append("<div><p id='paraAmount'>Invoice Amount : "+$("#inpAmount").val()+"</p><p id='paraTax0'>Sales/Purchase Tax @ 0% : 0</p></div><div><p id='paraTax5'>Sales/Purchase Tax @ 5% : "+$tax5+"</p><p id='paraTax12'>Sales/Purchase Tax @ 12% : "+$tax12+"</p></div><div><p id='paraTax18'>Sales/Purchase Tax @ 18% : "+$tax18+"</p><p id='paraTax28'>Sales/Purchase Tax @ 28% : "+$tax28+"</p></div><hr> <div id='GrandTotal'><p id='paraGrandTotal'>Grand Total : "+$gTotal+"</p></div>")
-    }
-}
-})
